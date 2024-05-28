3a2e063dbd133dd95707ffd269a05a594fa8c22d
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 14:40:34 -0500

    Add files via upload

---
+++ namespaceAssingment/namespaceAssingment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace namespaceAssingment
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is " + Name + " and my age is " + Age + ".");
+        }
+
+        public void newPerson()
+        {
+
+        }
+    }
+
+}
---
+++ namespaceAssingment/namespaceAssingment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace namespaceAssingment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "John";
+            person.Age = 28;
+            person.HasPet = true;
+
+            person.Greeting();
+            person.newPerson();
+        }
+    }
+}
